fd96e2b20af1b7b973412bce54a0085eb09662c9
Arya Rezza Anantya <43742778+anantyan@users.noreply.github.com>
2022-06-13 23:01:58 +0700

    First upload 🚀

---
+++ app/src/main/java/id/co/binar/secondhand/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package id.co.binar.secondhand.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.co.binar.secondhand.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
