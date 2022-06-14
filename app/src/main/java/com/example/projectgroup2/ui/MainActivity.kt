2d215d56c2fe578076c3c1a999f594553fa0fc1d
bagussatriakusuma <logocreatorfinn@gmail.com>
2022-06-14 17:41:11 +0700

    setup gradle, package, drawable, navigation, layout (login, register, profile)

--- app/src/main/java/com/example/projectgroup2/ui/MainActivity.kt
+++ app/src/main/java/com/example/projectgroup2/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.projectgroup2
+package com.example.projectgroup2.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.projectgroup2.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
